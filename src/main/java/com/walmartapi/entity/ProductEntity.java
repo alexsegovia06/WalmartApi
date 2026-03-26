bdbea72c7ecd5557d9772d7b04a58b228c86675e
Alejandro Segovia <alejandross@cetys.edu.mx>
2026-03-25 17:50:36 -0700

    Trabajo de clase

---
+++ src/main/java/com/walmartapi/entity/ProductEntity.java
@@ -0,0 +1,21 @@
+package com.walmartapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+}
